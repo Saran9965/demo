c38206239eb89f4ed0c00540b775405939d14757
Saran <saranvms2002@gmail.com>
2025-03-24 21:11:38 +0530

    invoice

--- invoice-form/project/src/app/app.component.ts
+++ invoice-form/project/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FrontendComponent } from './frontend/frontend.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[FrontendComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
